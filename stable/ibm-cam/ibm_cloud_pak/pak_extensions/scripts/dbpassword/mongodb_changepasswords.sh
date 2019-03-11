402e7728ba01461e40f6c01ee58372f2aef8cd9c
octravis <octravis@us.ibm.com>
2019-03-11 08:25:39 -0400

    Update master Mon Mar 11 03:02:12 UTC 2019 (#219)

--- stable/ibm-cam/ibm_cloud_pak/pak_extensions/scripts/dbpassword/mongodb_changepasswords.sh
+++ stable/ibm-cam/ibm_cloud_pak/pak_extensions/scripts/dbpassword/mongodb_changepasswords.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #
+# Licensed Materials - Property of IBM
+# 5737-E67
+# (C) Copyright IBM Corporation 2016-2019 All Rights Reserved.
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+#
 # This script will change the admin and camuser passwords
 #
 date
