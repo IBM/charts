54ab4154463e129a6b60930259ac45462f86acfa
octravis <octravis@us.ibm.com>
2019-07-08 11:59:08 -0400

    Update master Mon Jul  8 15:39:12 UTC 2019 (#350)

--- stable/ibm-was-vm-quickstarter-dev/ibm_cloud_pak/pak_extensions/post-delete/namespaceAdministration/deleteSecurityNamespacePrereqs.sh
+++ stable/ibm-was-vm-quickstarter-dev/ibm_cloud_pak/pak_extensions/post-delete/namespaceAdministration/deleteSecurityNamespacePrereqs.sh
@@ -19,7 +19,7 @@
 
 if [ "$#" -lt 1 ]; then
 	echo "Usage: deleteSecurityNamespacePrereqs.sh NAMESPACE"
-    exit 1
+  exit 1
 fi
 
 cd $(dirname $0)
@@ -27,7 +27,7 @@ cd $(dirname $0)
 namespace=$1
 
 # Replace the NAMESPACE tag with the namespace specified in a temporary yaml file.
-sed 's/{{ NAMESPACE }}/'$namespace'/g' ibm-was-vm-quickstarter-rb.yaml > $namespace-ibm-was-vm-quickstarter-rb.yaml
+sed 's/{{ NAMESPACE }}/'$namespace'/g' ../../pre-install/namespaceAdministration/ibm-was-vm-quickstarter-rb.yaml > $namespace-ibm-was-vm-quickstarter-rb.yaml
 
 # Delete the role binding for all service accounts in the current namespace
 kubectl delete -f $namespace-ibm-was-vm-quickstarter-rb.yaml -n $namespace
