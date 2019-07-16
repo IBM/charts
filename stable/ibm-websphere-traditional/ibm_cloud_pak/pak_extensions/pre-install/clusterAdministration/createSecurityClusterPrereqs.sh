701a1f98b1c47903bc4425414f77f945e4b4f833
OC Travis <octravis@us.ibm.com>
2019-07-16 23:52:23 +0000

    Update master Tue Jul 16 23:52:23 UTC 2019

--- stable/ibm-websphere-traditional/ibm_cloud_pak/pak_extensions/pre-install/clusterAdministration/createSecurityClusterPrereqs.sh
+++ stable/ibm-websphere-traditional/ibm_cloud_pak/pak_extensions/pre-install/clusterAdministration/createSecurityClusterPrereqs.sh
@@ -3,7 +3,7 @@
 #################################################################
 # Licensed Materials - Property of IBM
 # 5725-S17 IBM IoT MessageSight
-# (C) Copyright IBM Corp. 2018.  All Rights Reserved.
+# (C) Copyright IBM Corp. 2018, 2019.  All Rights Reserved.
 #
 # US Government Users Restricted Rights - Use, duplication or
 # disclosure restricted by GSA ADP Schedule Contract with
@@ -13,6 +13,17 @@
 # You need to run this script once prior to installing the chart.
 #
 
-# Create the PodSecurityPolicy and ClusterRole for all releases of this chart.
-kubectl apply -f ibm-websphere-traditional-psp.yaml
-kubectl apply -f ibm-websphere-traditional-cr.yaml
+. ../../common/kubhelper.sh
+
+if supports_scc; then 
+  # Create the custom SCC for OpenShift
+  echo "Creating SecurityContextConstraints..."
+  kubectl apply -f ibm-websphere-traditional-scc.yaml --validate=false
+fi
+
+if supports_psp; then 
+  # Create the PodSecurityPolicy and ClusterRole for all releases of this chart.
+  echo "Creating the PodSecurityPolicy..."
+  kubectl apply -f ibm-websphere-traditional-psp.yaml
+  kubectl apply -f ibm-websphere-traditional-cr.yaml
+fi
--- stable/ibm-websphere-traditional/ibm_cloud_pak/pak_extensions/pre-install/namespaceAdministration/createSecurityNamespacePrereqs.sh
+++ stable/ibm-websphere-traditional/ibm_cloud_pak/pak_extensions/pre-install/namespaceAdministration/createSecurityNamespacePrereqs.sh
@@ -3,7 +3,7 @@
 #################################################################
 # Licensed Materials - Property of IBM
 # 5725-S17 IBM IoT MessageSight
-# (C) Copyright IBM Corp. 2018.  All Rights Reserved.
+# (C) Copyright IBM Corp. 2019.  All Rights Reserved.
 #
 # US Government Users Restricted Rights - Use, duplication or
 # disclosure restricted by GSA ADP Schedule Contract with
@@ -18,6 +18,9 @@
 #     ./createSecurityNamespacePrereqs.sh myNamespace
 #
 
+. ../../common/kubhelper.sh
+
+
 if [ "$#" -lt 1 ]; then
 	echo "Usage: createSecurityNamespacePrereqs.sh NAMESPACE"
   exit 1
@@ -25,11 +28,29 @@ fi
 
 namespace=$1
 
-# Replace the NAMESPACE tag with the namespace specified in a temporary yaml file.
-sed 's/{{ NAMESPACE }}/'$namespace'/g' ibm-websphere-traditional-rb.yaml > $namespace-ibm-websphere-traditional-rb.yaml
+kubectl get namespace $namespace &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "ERROR: Namespace $namespace does not exist."
+  exit 1
+fi
 
-# Create the role binding for all service accounts in the current namespace
-kubectl create -f $namespace-ibm-websphere-traditional-rb.yaml -n $namespace
+if supports_scc; then
+  echo "Adding all namespace users to SCC..."
+  if command -v oc >/dev/null 2>&1 ; then
+    oc adm policy add-scc-to-group ibm-websphere-traditional-scc system:serviceaccounts:$namespace
+  else
+    echo "ERROR:  The OpenShift CLI is not available..." 
+  fi
+fi
 
-# Clean up - delete the temporary yaml file.
-rm $namespace-ibm-websphere-traditional-rb.yaml
+if supports_psp; then
+  # Replace the NAMESPACE tag with the namespace specified in a temporary yaml file.
+  sed 's/{{ NAMESPACE }}/'$namespace'/g' ibm-websphere-traditional-rb.yaml > $namespace-ibm-websphere-traditional-rb.yaml
+
+  echo "Adding a RoleBinding for all namespace users to the PSP..."
+  # Create the role binding for all service accounts in the current namespace
+  kubectl create -f $namespace-ibm-websphere-traditional-rb.yaml -n $namespace
+
+  # Clean up - delete the temporary yaml file.
+  rm $namespace-ibm-websphere-traditional-rb.yaml
+fi
