d97a67e27a3994bc263cc8c7a61c3a34559e86ad
octravis <octravis@us.ibm.com>
2019-07-16 20:10:22 -0400

    Update master Tue Jul 16 23:52:23 UTC 2019 (#362)

--- stable/ibm-websphere-traditional/ibm_cloud_pak/pak_extensions/post-delete/namespaceAdministration/deleteSecurityNamespacePrereqs.sh
+++ stable/ibm-websphere-traditional/ibm_cloud_pak/pak_extensions/post-delete/namespaceAdministration/deleteSecurityNamespacePrereqs.sh
@@ -3,7 +3,7 @@
 #################################################################
 # Licensed Materials - Property of IBM
 # 5725-S17 IBM IoT MessageSight
-# (C) Copyright IBM Corp. 2018.  All Rights Reserved.
+# (C) Copyright IBM Corp. 2019.  All Rights Reserved.
 #
 # US Government Users Restricted Rights - Use, duplication or
 # disclosure restricted by GSA ADP Schedule Contract with
@@ -18,6 +18,8 @@
 #     ./deleteSecurityNamespacePrereqs.sh myNamespace
 #
 
+. ../../common/kubhelper.sh
+
 if [ "$#" -lt 1 ]; then
 	echo "Usage: deleteSecurityNamespacePrereqs.sh NAMESPACE"
   exit 1
@@ -25,11 +27,29 @@ fi
 
 namespace=$1
 
-# Replace the NAMESPACE tag with the namespace specified in a temporary yaml file.
-sed 's/{{ NAMESPACE }}/'$namespace'/g' ibm-websphere-traditional-rb.yaml > $namespace-ibm-websphere-traditional-rb.yaml
+kubectl get namespace $namespace &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "ERROR: Namespace $namespace does not exist."
+  exit 1
+fi
 
-# Delete the role binding for all service accounts in the current namespace
-kubectl delete -f $namespace-ibm-websphere-traditional-rb.yaml -n $namespace
+if supports_scc; then
+  echo "Removing all namespace users from SCC..."
+  if command -v oc >/dev/null 2>&1 ; then
+    oc adm policy remove-scc-from-group ibm-websphere-traditional-scc system:serviceaccounts:$namespace
+  else
+    echo "ERROR:  The OpenShift CLI is not available..." 
+  fi
+fi
 
-# Clean up - delete the temporary yaml file.
-rm $namespace-ibm-websphere-traditional-rb.yaml
+
+if supports_psp; then
+  # Replace the NAMESPACE tag with the namespace specified in a temporary yaml file.
+  sed 's/{{ NAMESPACE }}/'$namespace'/g' ../../pre-install/namespaceAdministration/ibm-websphere-traditional-rb.yaml > $namespace-ibm-websphere-traditional-rb.yaml
+
+  # Delete the role binding for all service accounts in the current namespace
+  kubectl delete -f $namespace-ibm-websphere-traditional-rb.yaml -n $namespace
+
+  # Clean up - delete the temporary yaml file.
+  rm $namespace-ibm-websphere-traditional-rb.yaml
+fi
