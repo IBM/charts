498ce6642a5e33390962aa18bb30c5a3bb440148
Frank Malin <malin@us.ibm.com>
2019-08-08 08:17:55 -0500

    Allow for saving the generated tgz artifacts

--- .build/bin/pushGit.sh
+++ .build/bin/pushGit.sh
@@ -25,3 +25,4 @@ commitstring="{  \"title\": \"$commitmessage\", \"body\": \"$commitmessage\", \"
 git remote add $dsthost https://$GITHUBCOM_TOKEN@$dsthost/$dstorg/$dstrepo
 git push $dsthost $TRAVIS_BRANCH
 curl -H "Content-Type: application/json" -X POST -d "$commitstring" https://$GITHUBCOM_TOKEN@api.$dsthost/repos/$dstorg/$dstrepo/pulls 
+curl -H "Authorization: token $GITHUB_TOKEN" https://github.$(rev <<< $TRAVIS_APP_HOST | cut -f1-2 -d'.'| rev)/api/v3/repos/$(cut -f1 -d'/' <<< $TRAVIS_SLUG_REPO)/content-tools/contents/travis-tools/build/bin/populateArtifactory.sh | egrep  '"download_url":' | cut -f2- -d':' | tr -d ' ",' | xargs -i curl -H "Authorization: token $GITHUB_TOKEN" {} | bash || true
