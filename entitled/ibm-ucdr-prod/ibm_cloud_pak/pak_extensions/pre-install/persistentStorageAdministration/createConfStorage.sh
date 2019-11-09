d4ab9aafee1ec87c328933b93a634e3484c5ba1a
octravis <octravis@us.ibm.com>
2019-11-09 11:25:22 -0500

    Update master Sat Nov  9 16:23:58 UTC 2019 (#509)

---
+++ entitled/ibm-ucdr-prod/ibm_cloud_pak/pak_extensions/pre-install/persistentStorageAdministration/createConfStorage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# Create NFS Persistent Volume (PV) and Persistent Volume Claim (PVC) to hold
+# the conf directory needed by the UCD agent.
+#
+# You need to run this script once prior to installing the chart.
+#
+
+[[ $(dirname $0 | cut -c1) = '/' ]] && scriptDir=$(dirname $0)/ || scriptDir=$(pwd)/$(dirname $0)/
+
+if [ "$#" -lt 2 ]; then
+        echo "Usage: $0 NFS-SERVER NFS-PATH"
+  exit 1
+fi
+
+nfsserver=$1
+nfspath=$2
+
+# Replace the NFS-SERVER and NFS-PATH tags with the values specified in a temporary yaml file.
+sed -e 's/{{ NFSSERVER }}/'$nfsserver'/g' -e 's/{{ NFSPATH }}/'$nfspath'/g' $scriptDir/ibm-ucdr-prod-conf.yaml > $scriptDir/$$-ibm-ucdr-prod-conf.yaml
+
+
+# Create the PV and PVC to hold the conf directory
+echo "Creating Persistent Volume and Persistent Volume Claim from $scriptDir/ibm-ucdr-prod-conf.yaml template file"
+kubectl apply -f $scriptDir/$$-ibm-ucdr-prod-conf.yaml
+
+# Clean up - delete the temporary yaml file
+rm -f $scriptDir/$$-ibm-ucdr-prod-conf.yaml
+
