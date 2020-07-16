caa150a40584b4a8e1243f512ad24d7cd0c226a5
OC Travis <octravis@us.ibm.com>
2020-07-16 17:41:37 +0000

    Update master Thu Jul 16 17:41:37 UTC 2020

--- entitled/ibm-security-foundations-prod/ibm_cloud_pak/pak_extensions/pre-install/preInstall.sh
+++ entitled/ibm-security-foundations-prod/ibm_cloud_pak/pak_extensions/pre-install/preInstall.sh
@@ -14,68 +14,6 @@
 
 dir="$(dirname $0)"
 
-initSysctlOCP4() {
-# Init MachineConfigPool first, so KubeletConfig may find it
-  csys=$(kubectl get MachineConfigPool worker -o jsonpath="{.metadata.labels['custom-kubelet']}")
-  case "X$csys" in
-  Xsysctl)
-    ;;
-  X)
-        cat << EOF | kubectl apply -f -
-apiVersion: machineconfiguration.openshift.io/v1
-kind: MachineConfigPool
-metadata:
-  name: worker
-  labels:
-    custom-kubelet: sysctl
-EOF
-    ;;
-  *)
-    echo "ERROR: MachineConfigPool worker is already associated with custom-kubelet"
-    exit 1
-    ;;
-  esac   
-
-  sysc=$(kubectl get KubeletConfig custom-kubelet -o name 2>/dev/null)
-  if [ "X$sysc" == "X" ]; then
-cat << EOF | kubectl create -f -
-apiVersion: machineconfiguration.openshift.io/v1
-kind: KubeletConfig
-metadata:
-  name: custom-kubelet
-spec:
-  machineConfigPoolSelector:
-    matchLabels:
-      custom-kubelet: sysctl 
-  kubeletConfig:
-    allowedUnsafeSysctls:
-    - "net.core.somaxconn"
-EOF
-    if [ $? -ne 0 ]; then
-      echo "ERROR: create KubeletConfig has failed"
-      exit 1
-    fi
-  else
-    if [ $FORCE -eq 0 ]; then
-       echo "ERROR: KubeletConfig custom-kubelet already created"
-       exit 1
-    fi
-    kubectl patch KubeletConfig custom-kubelet --type merge --patch \
-'{"spec":{"machineConfigPoolSelector":{"matchLabels":{"custom-kubelet":"sysctl"}}}}'
-    kubectl patch KubeletConfig custom-kubelet --type merge --patch \
-'{"spec":{"kubeletConfig":{"allowedUnsafeSysctls":["net.core.somaxconn"]}}}'
-  fi
-
-}
-
-initSysctlOCP3() {
-  # update node configuration to enable reboots
-  for cm in $(kubectl get configmap -n openshift-node -o name)
-  do
-    editNodeConfig $cm
-  done
-}
-
 initSCC() {
   scc=$(kubectl get scc ibm-isc-scc -o name 2>/dev/null)
   if [ "X$scc" == "X" ]; then
@@ -95,10 +33,6 @@ initSCC() {
      fi
   fi
  
-  if [ $SYSCTL -ne 0 ]; then
-    checkSysctlsAllow
-  fi
-
 # allow execution as user 1001 and 8888
   echo "INFO: assocating accounts with scc"
   for acc in ambassador ibm-isc-operators ibm-isc-application \
@@ -161,41 +95,7 @@ setPullSecret() {
   fi
 }
 
-editNodeConfig() {
-  configmap="$(echo $1|sed -e 's!^.*/!!')"
-
-  kubectl get configmap -n openshift-node $configmap -o yaml \
-    > /tmp/${configmap}-$$.yaml
-  grep -q allowed-unsafe-sysctls: /tmp/${configmap}-$$.yaml
-  if [ $? -eq 0 ]; then
-    echo "${configmap} is already updated"
-    rm -f /tmp/${configmap}-$$.yaml
-    return
-  fi
-  sed -n '1,/kubeletArguments:/p' /tmp/${configmap}-$$.yaml > /tmp/out-${configmap}.$$
-  cat $dir/clusterAdministration/node-config.dat >> /tmp/out-${configmap}.$$
-  sed -e '1,/kubeletArguments:/d' /tmp/${configmap}-$$.yaml >> /tmp/out-${configmap}.$$
-
-  kubectl replace -f /tmp/out-${configmap}.$$
-  if [ $? -ne 0 ]; then
-     echo "ERROR: Failed to update configmap $configmap"
-     exit 1
-  else
-     echo "INFO: Node configmap $configmap has been updated"
-  fi
-  rm -f /tmp/${configmap}-$$.yaml /tmp/out-${configmap}.$$
-}
-
-checkSysctlsAllow() {
-  sccs=$(kubectl get scc ibm-isc-scc -o 'jsonpath={.allowedUnsafeSysctls}' 2>/dev/null)
-  if [ "X$sccs" == "X" ]; then
-    echo "The securitycontextconstraints/ibm-isc-scc created but the allowedUnsafeSysctls is not defined"
-    exit 1
-  fi
-}
-
-set_namespace()
-{
+set_namespace() {
   NAMESPACE="$1"
   ns=$(kubectl get namespace $NAMESPACE -o name 2>/dev/null)
   if [ "X$ns" == "X" ]; then
@@ -212,7 +112,6 @@ where args may be
 -n <NAMESPACE>     : if NAMESPACE is different from current
 -force             : force objects being overridden
 -repo <REPOSITORY> <USER> <PASSWORD>   : to specify the image repository
--sysctl            : to enable nondefault net.core.somaxconn sysctl
 -ibmcloud          : if installation is performed on IBM Cloud 
 -version           : OpenShift version (3.11, 4.2 or 4.3) to override autodetection
 EOF
@@ -246,13 +145,8 @@ do
       repository="$1"
       username="$2"
       password="$3"
-      shift
-      shift
-      shift
+      shift 3
       ;; 
-    -sysctl)
-      SYSCTL=1
-      ;;
     -ibmcloud)
       IBMCLOUD=1
       ;;
@@ -291,20 +185,5 @@ if [ "X$repository" != "X" ]; then
 fi
 
 initSCC
-
-if [ $SYSCTL -eq 1 ]; then
-  if [ $IBMCLOUD -ne 0 ]; then
-    echo "-sysctl flag is not used when in IBM Cloud"
-    exit 1
-  fi
-  case $version in
-    3.11)
-      initSysctlOCP3
-      ;;
-    *)
-      initSysctlOCP4
-      ;;
-  esac
-fi
 
 markNodes
