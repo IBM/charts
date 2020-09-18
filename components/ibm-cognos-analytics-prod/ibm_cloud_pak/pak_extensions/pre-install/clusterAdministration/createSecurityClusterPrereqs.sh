324510bc80b30c5cb74c44d391f317a057f47262
octravis <octravis@us.ibm.com>
2020-09-18 17:00:12 -0400

    Update master Fri Sep 18 20:39:38 UTC 2020 (#862)

--- components/ibm-cognos-analytics-prod/ibm_cloud_pak/pak_extensions/pre-install/clusterAdministration/createSecurityClusterPrereqs.sh
+++ components/ibm-cognos-analytics-prod/ibm_cloud_pak/pak_extensions/pre-install/clusterAdministration/createSecurityClusterPrereqs.sh
@@ -13,20 +13,20 @@ set -e
 # You need to run this script once prior to installing the chart.
 #
 
-. ../../common/kubhelper.sh
-
-
-if supports_scc; then
-  # Create the custom SCC for OpenShift
-  echo "Creating SecurityContextConstraints..."
-  oc apply -f ../../prereqs/ibm-cognos-analytics-prod-scc.yaml
-else
-  if supports_psp; then
-    # Create the PodSecurityPolicy and ClusterRole for all releases of this chart.
-    echo "Creating the PodSecurityPolicy..."
-    kubectl apply -f ibm-cognos-analytics-prod-psp.yaml
-    kubectl apply -f ibm-cognos-analytics-prod-cr.yaml
-  fi
-fi
-
-
+#. ../../common/kubhelper.sh
+#
+#
+#if supports_scc; then
+#  # Create the custom SCC for OpenShift
+#  echo "Creating SecurityContextConstraints..."
+#  oc apply -f ../../prereqs/ibm-cognos-analytics-prod-scc.yaml
+#else
+#  if supports_psp; then
+#    # Create the PodSecurityPolicy and ClusterRole for all releases of this chart.
+#    echo "Creating the PodSecurityPolicy..."
+#    kubectl apply -f ibm-cognos-analytics-prod-psp.yaml
+#    kubectl apply -f ibm-cognos-analytics-prod-cr.yaml
+#  fi
+#fi
+#
+#
--- components/ibm-cognos-analytics-prod/ibm_cloud_pak/pak_extensions/pre-install/clusterAdministration/createSecurityNamespacePrereqs.sh
+++ components/ibm-cognos-analytics-prod/ibm_cloud_pak/pak_extensions/pre-install/clusterAdministration/createSecurityNamespacePrereqs.sh
@@ -17,41 +17,41 @@ set -e
 # Example:
 #     ./createSecurityNamespacePrereqs.sh myNamespace
 #
-
-. ../../common/kubhelper.sh
-
-
-if [ "$#" -lt 1 ]; then
-	echo "Usage: createSecurityNamespacePrereqs.sh NAMESPACE"
-  exit 1
-fi
-
-namespace=$1
-
-kubectl get namespace $namespace &> /dev/null
-if [ $? -ne 0 ]; then
-  echo "ERROR: Namespace $namespace does not exist."
-  exit 1
-fi
-
-if supports_scc; then
-  echo "Adding all namespace users to SCC..."
-  if command -v oc >/dev/null 2>&1 ; then
-
-    # add scc to the service account needed for db2
-    echo "Adding scc required for db2 (cs) pod"
-    oc adm policy add-scc-to-user ibm-cognos-analytics-prod-scc system:serviceaccount:$namespace:cs-service-account
-
-  else
-    echo "ERROR:  The OpenShift CLI is not available..."
-  fi
-
-
-else
-  if supports_psp; then
-    echo "Adding psp .... "
-    # Replace the NAMESPACE tag with the namespace specified in a temporary yaml file
-    sed 's/<< NAMESPACE >>/'$namespace'/g' ibm-cognos-analytics-prod-rb.yaml | kubectl -n $namespace apply -f -
-  fi;
-fi
-
+#
+#. ../../common/kubhelper.sh
+#
+#
+#if [ "$#" -lt 1 ]; then
+#	echo "Usage: createSecurityNamespacePrereqs.sh NAMESPACE"
+#  exit 1
+#fi
+#
+#namespace=$1
+#
+#kubectl get namespace $namespace &> /dev/null
+#if [ $? -ne 0 ]; then
+#  echo "ERROR: Namespace $namespace does not exist."
+#  exit 1
+#fi
+#
+#if supports_scc; then
+#  echo "Adding all namespace users to SCC..."
+#  if command -v oc >/dev/null 2>&1 ; then
+#
+#    # add scc to the service account needed for db2
+#    echo "Adding scc required for db2 (cs) pod"
+#    oc adm policy add-scc-to-user ibm-cognos-analytics-prod-scc system:serviceaccount:$namespace:cs-service-account
+#
+#  else
+#    echo "ERROR:  The OpenShift CLI is not available..."
+#  fi
+#
+#
+#else
+#  if supports_psp; then
+#    echo "Adding psp .... "
+#    # Replace the NAMESPACE tag with the namespace specified in a temporary yaml file
+#    sed 's/<< NAMESPACE >>/'$namespace'/g' ibm-cognos-analytics-prod-rb.yaml | kubectl -n $namespace apply -f -
+#  fi;
+#fi
+#
