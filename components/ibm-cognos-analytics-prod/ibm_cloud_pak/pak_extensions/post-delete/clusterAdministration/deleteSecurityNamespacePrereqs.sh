6a83c616d5ad01a7ec92047e1588874afafce1ef
octravis <octravis@us.ibm.com>
2020-09-18 17:00:12 -0400

    Update master Fri Sep 18 20:39:38 UTC 2020 (#862)

--- components/ibm-cognos-analytics-prod/ibm_cloud_pak/pak_extensions/post-delete/clusterAdministration/deleteSecurityNamespacePrereqs.sh
+++ components/ibm-cognos-analytics-prod/ibm_cloud_pak/pak_extensions/post-delete/clusterAdministration/deleteSecurityNamespacePrereqs.sh
@@ -18,32 +18,35 @@ set -e
 #     ./deleteSecurityNamespacePrereqs.sh myNamespace
 #
 
-. ../../common/kubhelper.sh
-
-if [ "$#" -lt 1 ]; then
-	echo "Usage: deleteSecurityNamespacePrereqs.sh NAMESPACE"
-  exit 1
-fi
-
-namespace=$1
-
-kubectl get namespace $namespace &> /dev/null
-if [ $? -ne 0 ]; then
-  echo "ERROR: Namespace $namespace does not exist."
-  exit 1
-fi
-
-if supports_scc; then
-  echo "Removing all namespace users from SCC..."
-  if command -v oc >/dev/null 2>&1 ; then
-    echo "Openshift scc"
-    #oc adm policy remove-scc-from-group ibm-chart-dev-scc system:serviceaccounts:$namespace
-  else
-    echo "ERROR:  The OpenShift CLI is not available..." 
-  fi
-fi
-
-
-if supports_psp; then
-  sed 's/<< NAMESPACE >>/'$namespace'/g' ../../pre-install/clusterAdministration/ibm-cognos-analytics-prod-rb.yaml | kubectl -n $namespace delete -f -
-fi
+#. ../../common/kubhelper.sh
+#
+#if [ "$#" -lt 1 ]; then
+#	echo "Usage: deleteSecurityNamespacePrereqs.sh NAMESPACE"
+#  exit 1
+#fi
+#
+#namespace=$1
+#
+#kubectl get namespace $namespace &> /dev/null
+#if [ $? -ne 0 ]; then
+#  echo "ERROR: Namespace $namespace does not exist."
+#  exit 1
+#fi
+#
+#if supports_scc; then
+#  echo "Removing all namespace users from SCC..."
+#  if command -v oc >/dev/null 2>&1 ; then
+#    echo "Openshift scc"
+#    #oc adm policy remove-scc-from-group ibm-chart-dev-scc system:serviceaccounts:$namespace
+#  else
+#    echo "ERROR:  The OpenShift CLI is not available..."
+#  fi
+#
+#else
+#  if supports_psp; then
+#    echo "Removing cluster role binding ....."
+#    sed 's/<< NAMESPACE >>/'$namespace'/g' ../../pre-install/clusterAdministration/ibm-cognos-analytics-prod-rb.yaml | kubectl -n $namespace delete -f -
+#  fi
+#fi
+#
+#
--- components/ibm-cognos-analytics-prod/templates/pod-security-context.tpl
+++ components/ibm-cognos-analytics-prod/templates/pod-security-context.tpl
@@ -1,9 +1,11 @@
 {{- define "podSecurityContext" }}
   runAsNonRoot: {{ .Values.global.pod.ibmuser.securityContext.runAsNonRoot }}
-  {{- if not (.Capabilities.APIVersions.Has "security.openshift.io/v1") }}
+  {{- if and (not .Values.global.icp4Data) (not (.Capabilities.APIVersions.Has "security.openshift.io/v1")) }}
   fsGroup: {{ .Values.global.pod.ibmuser.securityContext.fsGroup }}
   runAsUser: {{ .Values.global.pod.ibmuser.securityContext.runAsUser }}
   {{- end }}
   supplementalGroups:
   {{ toYaml .Values.global.pod.ibmuser.securityContext.supplementalGroups }}
 {{- end }}
+
+
