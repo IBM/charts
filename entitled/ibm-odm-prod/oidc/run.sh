c9546963fd9d2a54db25b5b107aa7e1d2736367c
octravis <octravis@us.ibm.com>
2020-12-11 14:24:51 -0500

    Autocommit initiated by frankmalin
    Completed stage one of the testcase

---
+++ entitled/ibm-odm-prod/oidc/run.sh
@@ -0,0 +1,5 @@
+    #!/bin/sh
+    echo "Register the provided redirect Uris list with the provided or generated Client Id"
+    /oidc/registration.sh
+    echo "List the redirect Uris of the provided or generated Client Id"
+    /oidc/list.sh
