494314473af0327646ac572ad8424f2078a61083
octravis <octravis@us.ibm.com>
2021-06-24 10:30:42 -0400

    Autocommit initiated by frankmalin
    Completed stage one of the testcase

--- entitled/ibm-odm-prod/oidc/delete.sh
+++ entitled/ibm-odm-prod/oidc/delete.sh
@@ -1,9 +1,7 @@
-   #!/bin/bash
+#!/bin/bash
 
-   OIDC_SERVER_URL=$(cat /server_url/server_url.properties)
-   OIDC_PROVIDER=$(cat /provider/provider.properties)
-   OIDC_CLIENT_ID=$(cat /oidc-client-id/oidc-client-id)
-   OIDC_USERNAME=$(cat /oidc-username/oidc-username)
-   OIDC_PASSWORD=$(cat /oidc-password/oidc-password)
-   echo "delete URL : ${OIDC_SERVER_URL}/oidc/endpoint/${OIDC_PROVIDER}/registration/${OIDC_CLIENT_ID}"
-   curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -k -u ${OIDC_USERNAME}:${OIDC_PASSWORD} --request DELETE ${OIDC_SERVER_URL}/oidc/endpoint/${OIDC_PROVIDER}/registration/${OIDC_CLIENT_ID} --insecure
+# Set OIDC variables
+. /oidc/common.sh
+
+echo "delete URL : ${OIDC_SERVER_URL}/oidc/endpoint/${OIDC_PROVIDER}/registration/${OIDC_CLIENT_ID}"
+curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -k -u ${OIDC_USERNAME}:${OIDC_PASSWORD} --request DELETE ${OIDC_SERVER_URL}/oidc/endpoint/${OIDC_PROVIDER}/registration/${OIDC_CLIENT_ID} --insecure
