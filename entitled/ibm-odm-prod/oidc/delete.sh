8dbf42da04d7cd7f99799586a230dc344711bb38
OC Travis <octravis@us.ibm.com>
2021-12-17 00:38:28 +0000

    Update master Fri Dec 17 00:38:28 UTC 2021

--- entitled/ibm-odm-prod/oidc/delete.sh
+++ entitled/ibm-odm-prod/oidc/delete.sh
@@ -3,5 +3,5 @@
 # Set OIDC variables
 . /oidc/common.sh
 
-echo "delete URL : ${OIDC_SERVER_URL}/oidc/endpoint/${OIDC_PROVIDER}/registration/${OIDC_CLIENT_ID}"
-curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -k -u ${OIDC_USERNAME}:${OIDC_PASSWORD} --request DELETE ${OIDC_SERVER_URL}/oidc/endpoint/${OIDC_PROVIDER}/registration/${OIDC_CLIENT_ID} --insecure
+echo "delete URL : ${OIDC_REGISTRATION_URL}/${OIDC_CLIENT_ID}"
+curl --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -k -u ${OIDC_USERNAME}:${OIDC_PASSWORD} --request DELETE ${OIDC_REGISTRATION_URL}/${OIDC_CLIENT_ID} --insecure
