a561f00b5345b7f831a6922de80f497aa2bc16a7
camgit <32468129+camgit@users.noreply.github.com>
2022-06-24 04:30:52 -0500

    Autocommit initiated by frankmalin
    Completed stage one of the testcase

--- entitled/ibm-odm-prod/tests/suite.sh
+++ entitled/ibm-odm-prod/tests/suite.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
     if [ $DECISION_RUNNER_ENABLED = "true" ]
     then
-      sh /tests/wait-for-url.sh $DECISION_RUNNER_NAME
+      sh /tests/wait-for-url.sh $PROTOCOL://$DECISION_RUNNER_NAME:9443/DecisionRunner
     fi
 
     if [ $DECISION_SERVER_RUNTIME_ENABLED = "true" ]
     then
-      sh /tests/wait-for-url.sh $DECISION_SERVER_RUNTIME_NAME resExecutor resExecutor
+      sh /tests/wait-for-url.sh $PROTOCOL://$DECISION_SERVER_RUNTIME_NAME:9443/DecisionService resExecutor resExecutor
     fi
 
     if [ $DECISION_CENTER_ENABLED = "true" ]
     then
-      sh /tests/wait-for-url.sh $DECISION_CENTER_NAME/decisioncenter/t
-      sh /tests/wait-for-url.sh $DECISION_CENTER_NAME/decisioncenter/assets/decision-center-client-api.zip
+      sh /tests/wait-for-url.sh $PROTOCOL://$DECISION_CENTER_NAME:9453/decisioncenter/t
+      sh /tests/wait-for-url.sh $PROTOCOL://$DECISION_CENTER_NAME:9453/decisioncenter/assets/decision-center-client-api.zip
     fi
 
     if [ $DECISION_SERVER_CONSOLE_ENABLED = "true" ]
     then
-      sh /tests/wait-for-url.sh $DECISION_SERVER_CONSOLE_NAME
+      sh /tests/wait-for-url.sh $PROTOCOL://$DECISION_SERVER_CONSOLE_NAME:9443/res
     fi
